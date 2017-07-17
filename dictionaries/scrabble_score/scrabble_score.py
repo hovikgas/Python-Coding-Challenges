1ca3770f08baba3540d1a6e53c6edb2537810d35
allardbrain <allardbrain@gmail.com>
2017-07-16 21:00:37 -0700

    Replaced with correct challenge. Added docstrings and upgraded solution.

--- dictionaries/scrabble_score/scrabble_score.py
+++ dictionaries/scrabble_score/scrabble_score.py
@@ -1,27 +1,51 @@
 '''
-Write a function called reverse() that takes in a string and returns that string in reverse.
+From the Codecademy Python track.
 
-You may not use reversed() or [::-1] to help you with this.
-You may get a string containing special characters (for example, !, @, or #).
+Scrabble is a game where players get points by spelling words. 
+Words are scored by adding together the point values of each individual letter. 
+Leave out the double and triple letter and word scores for now.
 
->>> reverse("Hello!")
-'!olleH'
+A dictionary containing all of the letters in the alphabet 
+with their corresponding Scrabble point values is provided.
 
->>> reverse("I love smoothies")
-'seihtooms evol I'
+For example: the word "Helix" would score 15 points 
+due to the sum of the letters: 4 + 1 + 1 + 1 + 8.
+
+Write a function called scrabble_score() that takes a word (as a string) as input 
+and returns the equivalent scrabble score for that word.
+
+Assume your input is only one word containing only letters.
+Your function should work even if the letters you get are uppercase, lowercase, or a mix.
+Assume that you're only given non-empty strings.
+
+>>> scrabble_score("orange")
+7
+
+>>> scrabble_score("NINJA")
+12
+
+>>> scrabble_score("Write")
+8
 
 '''
 
-def reverse(text):
-    
-    reversed_str = ""
-    
-    for i in range((len(text) - 1), -1, -1):
-        reversed_str += text[i]
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
 
-    # print("Original string: ", text)
-    # print("Reversed string: ", reversed_str)
-    return reversed_str
+         
+def scrabble_score(word):
+
+    word_score = 0
+    word = word.lower()
+    
+    for char in word:
+        word_score += score[char]
+
+    return word_score
+
 
 
 if __name__ == '__main__':
