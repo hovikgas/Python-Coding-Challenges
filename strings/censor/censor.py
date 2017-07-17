d10601faea15d296461ea2b9d3dd233ccbf44642
allardbrain <allardbrain@gmail.com>
2017-07-16 20:52:37 -0700

    Added doctest and upgraded solution.

--- strings/censor/censor.py
+++ strings/censor/censor.py
@@ -1,10 +1,15 @@
 '''
-From the Codecademy Python track.
-
-Write a function called censor that takes two strings, text and word, as input. 
-It should return the text with the word you chose replaced with asterisks. 
-Assume your input strings won't contain punctuation or upper case letters.
+Write a function called censor() that takes two strings, text (a multi-word string) and word (a one-word
+string), as input. It should return the text with the word you specified replaced with asterisks. 
+Assume your input strings will only contain lowercase letters.
 The number of asterisks you put should correspond to the number of letters in the censored word.
+
+>>> censor("my dog eats cupcakes", "cupcakes")
+'my dog eats ********'
+
+>>> censor("I lost my glasses", "lost")
+'I **** my glasses'
+
 '''
 
 def censor(text, word):
@@ -17,6 +22,13 @@ def censor(text, word):
             split_words[split_words.index(each)] = stars
             
     censored_text = " ".join(split_words)
+
     return censored_text
 
+
+censor("It's peach season", "peach")
+
             
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
