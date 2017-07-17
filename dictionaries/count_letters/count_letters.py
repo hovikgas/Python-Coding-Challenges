e4905c5293ee066062cf3b3fd6a287ff4e71de21
allardbrain <allardbrain@gmail.com>
2017-07-16 20:24:12 -0700

    Added doctests and upgraded solution.

--- dictionaries/count_letters/count_letters.py
+++ dictionaries/count_letters/count_letters.py
@@ -1,6 +1,13 @@
 '''
-Write a function that takes in a string argument, and returns a dictionary containing
+Write a function count_chars() that takes in a string argument, and returns a dictionary containing
 the characters in the string, along with the number of times each character appears in the string.
+
+>>> count_chars("Baby") == {'a': 1, 'b': 1, 'y': 1, 'B': 1}
+True
+
+>>> count_chars("Hello!") == {'!': 1, 'e': 1, 'l': 2, 'o': 1, 'H': 1}
+True
+
 '''
 
 def count_chars(my_string):
@@ -18,7 +25,12 @@ def count_chars(my_string):
             char_count[temp_char] = 1
 
 
-    print("Character counts: ", char_count)
-    return char_count;
+    # print("Character counts: ", char_count)
+    return char_count
 
 count_chars("Ahoy! I am a pirate! $ @ 4 5 5 5 { { {");
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
