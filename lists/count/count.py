3c9585224a40c9a43afcaea0d0223795f701d2d2
allardbrain <allardbrain@gmail.com>
2017-07-19 07:44:33 -0700

    Added doctests.

--- lists/count/count.py
+++ lists/count/count.py
@@ -1,15 +1,28 @@
 '''
-From the Codecademy Python track. 
+Define a function called count() that has two arguments, called sequence (a list) and item.
+The item you input may be an integer, string or float.
+Return an integer representing the number of times the item occurs in the sequence.
 
-Define a function called count that has two arguments called sequence and item.
-Return the number of times the item occurs in the list.
-For example: count([1,2,1,1], 1) should return 3 (because 1 appears 3 times in the list).
-Your function should return an integer.
-The item you input may be an integer, string, float, or even another list!
+>>> count([1, 4, 1, 3, 1, 6], 1)
+3
+
+>>> count(["boat", "kayak", 1, 3, "boat"], "boat")
+2
 '''
 def count(sequence, item):
+    
     result = 0
+        
     for i in sequence:
         if i == item:
             result += 1
+
     return result
+
+
+count(["hello", "poodle", "chocolate"], "chocolate")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
