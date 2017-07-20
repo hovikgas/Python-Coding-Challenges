415cbf898276d6802b1e6e2a32ad8b106f81d52e
allardbrain <allardbrain@gmail.com>
2017-07-19 18:30:10 -0700

    Upgraded solution and added doctests.

--- numbers/factorial/factorial.py
+++ numbers/factorial/factorial.py
@@ -1,13 +1,41 @@
 '''
-From the Codecademy Python track.
+Define a function factorial() that takes an integer x as input
+and returns the factorial of x. A factorial of a number is 
+calculated as a number multiplied by all the numbers that are 
+less than it. Example:
 
-Define a function factorial that takes an integer x as input.
-Calculate and return the factorial of that number.
+5! = 5 * 4 * 3 * 2 * 1
+
+1! and 0! should both return 1.
+
+>>> factorial(5)
+120
+
+>>> factorial(0)
+1
+
+>>> factorial(1)
+1
+
 '''
 
 def factorial(x):
+
     product = 1
-    while x >= 1:
+
+    if x == 1 or x == 0:
+        return product
+
+    while x > 1:
         product *= x
         x -= 1
+
     return product
+
+
+factorial(3)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
