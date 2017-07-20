181988f30a0c2ed25d0faaf8fda80428117299b1
allardbrain <allardbrain@gmail.com>
2017-07-19 17:05:59 -0700

    Upgraded solution and added doctests.

--- lists/median/median.py
+++ lists/median/median.py
@@ -1,21 +1,37 @@
 '''
-Write a function called median that takes a list 
-as an input and returns the median value of the list.
+Write a function called median() that takes a list of numbers 
+as an argument and returns the median value of the list.
 
-For example: median([1,1,2]) should return 1.
+The median value is determined by choosing the middle number in a sorted list.
+If the list contains an even number of items, the median is the average of the middle two.
 
-The list can be of any size and the numbers are not 
-guaranteed to be in any particular order.
-If the list contains an even number of elements, 
-your function should return the average of the middle two.
+>>> median([1,1,2]) 
+1
+
+>>> median([1, 2, 3, 4])
+2.5
+
 '''
 
 def median(num_list):
     
     sorted_list = sorted(num_list)
-    print sorted_list
-    L = len(sorted_list)
-    if L % 2 == 0:
-        return (sorted_list[L / 2 - 1] + sorted_list[L / 2]) / 2.0 
+    length_sorted_list = len(sorted_list)
+
+    if length_sorted_list % 2 == 0:
+
+        # Use // for division or else you get an error for indexing a float
+        # See: https://stackoverflow.com/questions/1282945/python-integer-division-yields-float
+        return (sorted_list[(length_sorted_list // 2) - 1] + sorted_list[length_sorted_list // 2]) / 2 
+    
     else:   
-        return sorted_list[(L / 2)]  
+        return sorted_list[(length_sorted_list // 2)]  
+
+
+median([5, 6, 7])
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
