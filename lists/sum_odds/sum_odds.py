ff0c930631abeb33da0b3608cfd1bc58cc743d60
allardbrain <allardbrain@gmail.com>
2017-07-19 17:28:07 -0700

    Added doctests and upgraded solution.

--- lists/sum_odds/sum_odds.py
+++ lists/sum_odds/sum_odds.py
@@ -1,25 +1,35 @@
 '''
-Write a function that takes in an array of integers 
+Write a function called oddball_sum() that takes in a list of integers 
 and returns the sum of the odd elements.
 
-Test cases:
+>>> oddball_sum([1,2,3,4,5])
+9
 
-oddball_sum([1,2,3,4,5]) == 9 # => 1 + 3 + 5 == 9
-oddball_sum([0,6,4,4]) == 0
-oddball_sum([1,2,1]) == 2
+>>> oddball_sum([0,6,4,4])
+0
+
+>>> oddball_sum([1,2,1])
+2
+
 '''
 
 def oddball_sum(nums):
 
-    i = 0
     result = 0
 
     for i in range(len(nums)):
 
         if nums[i] % 2 == 1:
             result += nums[i]
-
-    print result  
+ 
     return result
+
+
+oddball_sum([17, 15, 0])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
 
