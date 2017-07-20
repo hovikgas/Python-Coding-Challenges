1d74694d6a3c7f674f39158c5c2f1d9537624c3e
allardbrain <allardbrain@gmail.com>
2017-07-19 17:25:25 -0700

    Added doctests.

--- lists/reverse/reverse_list.py
+++ lists/reverse/reverse_list.py
@@ -1,5 +1,13 @@
-''' Write a function that takes in a list and returns a new, reversed version of the list.
+''' 
+Write a function that takes in a list and returns a new, reversed version of the list.
 Do not use the list object's .reverse() method.
+
+>>> reverse_array([1, 2, 3, 4, 5])
+[5, 4, 3, 2, 1]
+
+>>> reverse_array(['apple', 'boat', 'cat', 'dog'])
+['dog', 'cat', 'boat', 'apple']
+
 '''
 
 def reverse_array(my_list):
@@ -9,8 +17,11 @@ def reverse_array(my_list):
     for i in range((len(my_list) - 1), -1, -1):
         reversed_list.append(my_list[i])
 
-    print("Original list: ", my_list)
-    print("Reversed list: ", reversed_list)
     return reversed_list
 
 reverse_array([1, 2, 3, 4, 5]);
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
