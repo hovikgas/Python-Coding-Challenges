9717507d3f741f252e067d6d279e2d53ad79cd5e
allardbrain <allardbrain@gmail.com>
2017-07-19 17:13:27 -0700

    Added doctests

--- lists/purify/purify.py
+++ lists/purify/purify.py
@@ -1,16 +1,31 @@
 '''
-From the Codeceademy Python track
+Define a function called purify that takes in a list of numbers. 
+Return a new list that contains only the even numbers from the input list.
+Assume zero is an even number.
 
-Define a function called purify that takes in a list of numbers, 
-removes all odd numbers in the list, and returns the result.
-For example, purify([1,2,3]) should return [2].
-Do not directly modify the list you are given as input; 
-instead, return a new list with only the even numbers.
+>>> purify([1,2,3])
+[2]
+
+>>> purify([0, 100, 101, 201])
+[0, 100]
+
 '''
 
 def purify(numbers):
+
     result = []
+
     for i in range(len(numbers)):
+
         if numbers[i] % 2 == 0:
             result.append(numbers[i])
+
     return result
+
+
+purify([5, 10, 15, 20, 25, 30])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
