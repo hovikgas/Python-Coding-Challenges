97bac2e6621eb669cc5558eae24abf6713b828f9
allardbrain <allardbrain@gmail.com>
2017-07-19 18:57:25 -0700

    Cleaned up prompt, upgraded solution & added doctests.

--- numbers/is_prime/is_prime.py
+++ numbers/is_prime/is_prime.py
@@ -1,10 +1,17 @@
 '''
-From the Codecademy Python track.
-
-Define a function called is_prime that takes a number x as input.
+Define a function called is_prime() that takes a number, x, as input.
 For each number n from 2 to x - 1, test if x is evenly divisible by n.
-If it is, return False.
-If none of them are, then return True.
+If x is evenly divisible by n, return False, since prime numbers are 
+only divisible by themselves and 1. If x is not evenly divisible by n, 
+then return True, since prime numbers are not divisible by anything but 
+themselves and 1.
+
+>>> is_prime(27)
+False
+
+>>> is_prime(5)
+True
+
 '''
 
 def is_prime(x):
@@ -12,11 +19,17 @@ def is_prime(x):
     if x < 2:
         return False
         
-    for n in range(2, x):
+    for n in range(2, x - 1):
         if x % n == 0:
             return False
-            break
-    else: return True
 
+    return True
+
+is_prime(3)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
             
 
