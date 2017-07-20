b86c1b488100a0043ec3adae1d897d86963cfe65
allardbrain <allardbrain@gmail.com>
2017-07-19 18:37:17 -0700

    Fixed prompt expected output and added doctest.

--- numbers/fizzbuzz/fizzbuzz.py
+++ numbers/fizzbuzz/fizzbuzz.py
@@ -1,14 +1,19 @@
-''' Write a funciton called fizzbuzz, which takes a number (n) as an argument and returns a list. 
-For each integer between 1 and n, if the integer is divisible by 3, append "buzz" 
-to the result list. If the integer is divisible by 5, append "fizz" to the result list. 
-If the integer is divisible by both 3 and 5, append "fizzbuzz" to the result list. 
+''' Write a funciton called fizzbuzz() which takes a number (n) as an argument and returns a list. 
+For each integer between 1 and n, if the integer is divisible by 3, append "Fizz" 
+to the result list. If the integer is divisible by 5, append "Buzz" to the result list. 
+If the integer is divisible by both 3 and 5, append "FizzBuzz" to the result list. If the integer
+is not evenly divisible by 3 or 5, append the integer itself to the result list.
+
+>>> fizzbuzz(15)
+[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+
 '''
 
 def fizzbuzz(n):
 
     result = []
 
-    for i in range(1, n):
+    for i in range(1, n + 1):
 
         if i % 3 == 0 and i % 5 == 0:
             result.append('FizzBuzz')
@@ -22,11 +27,15 @@ def fizzbuzz(n):
         else:
             result.append(i)
 
-    print(result)
     return result
 
 
-fizzbuzz(99)
+fizzbuzz(100)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
 
     
