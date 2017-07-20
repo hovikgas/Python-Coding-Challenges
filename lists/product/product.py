35cc159e6fcdf639b011667a4f95cb647524dbe3
allardbrain <allardbrain@gmail.com>
2017-07-19 17:09:55 -0700

    Added doctests.

--- lists/product/product.py
+++ lists/product/product.py
@@ -1,14 +1,29 @@
 '''
-From the Codecademy Python track
+Define a function called product() that takes a list of integers as input 
+and returns the product of all of the elements in the list as an integer.
+Assume the list has at least one element in it.
 
-Define a function called product that takes a list of integers as input 
-and returns the product of all of the elements in the list.
-For example: product([4, 5, 5]) should return 100 (because 4 * 5 * 5 is 100).
-Don't worry about the list being empty. Your function should return an integer.
+>>> product([5])
+5
+
+>>> product([4, 5, 5])
+100
+
 '''
 
 def product(integers):
+
     result = 1
+
     for i in range(len(integers)):
         result *= integers[i]
+
     return result
+
+
+product([3, 4, 5])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod
