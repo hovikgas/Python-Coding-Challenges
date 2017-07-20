e96ce01ac43783ea6f2363f2432343916f85eea4
allardbrain <allardbrain@gmail.com>
2017-07-19 18:49:24 -0700

    Added doctests.

--- numbers/is_even/is_even.py
+++ numbers/is_even/is_even.py
@@ -1,13 +1,27 @@
 '''
-From the Codecademy Python track.
+Define a function is_even() that will take a number, x, as input.
+If x is even, return True. Otherwise, return False.
 
-Define a function is_even that will take a number x as input.
-If x is even,  return True.
-Otherwise, return False.
+>>> is_even(6)
+True
+
+>>> is_even(5)
+False
+
 '''
 
 def is_even(x):
+
     if x % 2 == 0:
         return True
+
     else: 
         return False
+
+
+is_even(10)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
